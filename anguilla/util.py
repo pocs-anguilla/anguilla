2b181478a7ac5deb19551d30cd32fb41b2028726
pocs-anguilla <74969248+pocs-anguilla@users.noreply.github.com>
2020-12-02 05:19:11 +0100

    Add a generator for random orthogonal matrices (#7)

--- anguilla/util.py
+++ anguilla/util.py
@@ -4,6 +4,9 @@ import functools
 
 from typing import Callable
 
+import numba
+import numpy as np
+
 
 def _exp_norm_chi_implementation(
     f: Callable[[float], float]
@@ -67,3 +70,74 @@ def exp_norm_chi(k: float) -> float:
         compute the approximation.
     """
     pass
+
+
+def random_orthogonal_matrix(n: int, rng: np.random.Generator) -> np.ndarray:
+    """Generate a random orthogonal matrix with distribution given by a \
+    Haar measure.
+
+    Parameters
+    ----------
+    n
+        Size of the matrix.
+    rng
+        A random number generator.
+
+    Returns
+    -------
+    np.ndarray
+        The random orthogonal matrix.
+
+    Notes
+    -----
+    Implements the algorithm used in :cite:`2008:shark`, where it was chosen \
+    because it is faster [#f1]_ than an alternative algorithm that uses the \
+    QR decomposition and corrects the signs. See p. 16 from \
+    :cite:`2007:random-matrices`. We port the implementation here.
+
+    Some benchmark functions (e.g. CIGTAB1) require an orthogonal rotation \
+    matrix :cite:`2007:mo-cma-es`, so it suffices to use this variant over \
+    others that produce unitary random matrices.
+
+    .. [#f1] In our experiments, approximately twice as fast.
+    """
+    v = rng.standard_normal((n * n + n - 2) // 2)
+    return _random_orthogonal_matrix_jit(n, v)
+
+
+@numba.njit
+def _random_orthogonal_matrix_jit(n: int, v: np.ndarray) -> np.ndarray:
+    """The optimized algorithm implementation for \
+    :py:mod:`random_orthogonal_matrix`.
+    
+    Parameters
+    ----------
+    n
+        Size of the matrix.
+    v
+        Vector with (n^2 + n - 2) / 2 realizations of the starndard normal \
+        distribution.
+
+    Returns
+    -------
+    np.ndarray
+        The random orthogonal matrix.
+    """
+    Q = np.eye(n)
+    k = 0
+    # We can safely skip the first iteration
+    for i in range(2, n + 1):
+        # Compute v_hat
+        ki = k + i
+        ni = n - i
+        v[k:ki] /= np.linalg.norm(v[k:ki])
+        # Compute u_hat
+        sgn = v[k] / abs(v[k])
+        v[k] += sgn
+        tmp = v[k:ki]
+        beta = 2.0 / np.sum(tmp * tmp)
+        # Apply the Householder reflection
+        Q[ni:n, ni:n] -= beta * np.outer(tmp, Q[ni:n, ni:n].T @ tmp)
+        Q[ni:n, ni:n] *= -sgn
+        k += i
+    return Q
