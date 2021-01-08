8b71061ba8dae58003e56f4576c02437500aae83
Anguilla anguilla <pocs.anguilla@gmail.com>
2021-01-08 13:21:48 +0100

    Fix

--- conda-recipes/anguilla/build.sh
+++ conda-recipes/anguilla/build.sh
@@ -2,7 +2,7 @@ mkdir build
 cd build
 
 # Configure.
-cmake -DCMAKE_TOOLCHAIN_FILE=${SRC_DIR}/clang-cxx20-toolchain.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=${SRC_DIR}/cmake/toolchains/clang-cxx20-libcxx.cmake \
       -DCMAKE_BUILD_TYPE=Release \
       -GNinja \
       ${SRC_DIR};
