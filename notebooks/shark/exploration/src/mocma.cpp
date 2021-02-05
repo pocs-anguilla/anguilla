8f6718636e983689986e42d72bdb8d0eaa1ceca4
Anguilla anguilla <pocs.anguilla@gmail.com>
2021-02-05 22:55:57 +0100

    Add seed as parameter

--- notebooks/shark/exploration/src/mocma.cpp
+++ notebooks/shark/exploration/src/mocma.cpp
@@ -3,6 +3,7 @@
 #include <shark/Algorithms/DirectSearch/MOCMA.h>
 #include <shark/Algorithms/DirectSearch/Operators/Hypervolume/HypervolumeCalculator.h>
 #include <shark/Algorithms/DirectSearch/SteadyStateMOCMA.h>
+#include <shark/Core/Random.h>
 #include <shark/ObjectiveFunctions/Benchmarks/Benchmarks.h>
 #include <stdlib.h>
 
@@ -34,7 +35,8 @@ struct PointExtractor {
 template <typename Optimizer = MOCMA>
 class PopulationPlotExperiment {
    public:
-    static void run(int mu, int n, int maxEvaluations, RealVector *reference = nullptr, bool individual = false, std::string extra = "1|C", int instance = 1, std::size_t maxTrials = 3) {
+    static void run(int seed, int mu, int n, int maxEvaluations, RealVector *reference = nullptr, bool individual = false, std::string extra = "1|C", int instance = 1, std::size_t maxTrials = 3) {
+        random::globalRng().seed(seed);
         std::cout.setf(std::ios_base::scientific);
         std::cout.precision(10);
         std::vector<std::string> formats = {"xb", "xg", "xy"};
@@ -43,8 +45,8 @@ class PopulationPlotExperiment {
         std::string st = individual ? "I" : "P";
         auto nTrials = std::min(formats.size(), maxTrials);
         for (int i = 0; i < nTrials; i++) {
-            //benchmarks::DTLZ3 fn(n);
-            MOBenchmark fn(extra, n, instance);
+            benchmarks::CIGTAB2 fn(n);
+            //MOBenchmark fn(extra, n, instance);
             if (fn.hasScalableObjectives()) {
                 fn.setNumberOfObjectives(2);
             }
@@ -52,10 +54,10 @@ class PopulationPlotExperiment {
                 fn.setNumberOfVariables(n);
             }
 
-            if (i == 0) {
+            /*if (i == 0) {
                 auto [frontY1, frontY2] = fn.paretoFront(50);
                 plt::plot(frontY1, frontY2, "r-");
-            }
+            }*/
 
             Optimizer optimizer;
             if (individual) {
@@ -92,29 +94,30 @@ class PopulationPlotExperiment {
             }
             if (i + 1 == nTrials) {
                 plt::title(boost::str(boost::format("%1%(n=%2%), %3%-%4%\nmu=%5%,evals=%6%") % fn.name() % n % optimizer.name() % st % mu % fn.evaluationCounter()));
-                plt::save(boost::str(boost::format("./%1%n%2%-%3%-%4%-mu%5%-fe%6%.png") % safe_name(fn.name()) % n % optimizer.name() % st % mu % fn.evaluationCounter()));
+                plt::save(boost::str(boost::format("./%1%n%2%-%3%-%4%-mu%5%-fe%6%-seed%7%.png") % safe_name(fn.name()) % n % optimizer.name() % st % mu % fn.evaluationCounter() % seed));
             }
         }
     }
 };
 
 int main(int argc, char *argv[]) {
-    int mu = argc > 1 ? std::atoi(argv[1]) : 10;
-    int n = argc > 2 ? std::atoi(argv[2]) : 5;
-    int maxEvaluations = argc > 3 ? std::atoi(argv[3]) : 5000;
-    bool useSteadyState = argc > 4 ? std::strcmp("steady", argv[4]) == 0 : false;
-    bool individual = argc > 5 ? std::strcmp("individual", argv[5]) == 0 : false;
-    std::string extra = argc > 6 ? std::string(argv[6]) : "1|C";
-    int instance = argc > 7 ? std::atoi(argv[7]) : rand();
+    int seed = argc > 1 ? std::atoi(argv[1]) : 0;
+    int mu = argc > 2 ? std::atoi(argv[2]) : 10;
+    int n = argc > 3 ? std::atoi(argv[3]) : 5;
+    int maxEvaluations = argc > 4 ? std::atoi(argv[4]) : 5000;
+    bool useSteadyState = argc > 5 ? std::strcmp("steady", argv[5]) == 0 : false;
+    bool individual = argc > 6 ? std::strcmp("individual", argv[6]) == 0 : false;
+    std::string extra = argc > 7 ? std::string(argv[7]) : "1|C";
+    int instance = argc > 8 ? std::atoi(argv[8]) : rand();
 
     RealVector reference = {11.0, 11.0};
     RealVector *reference_ptr = nullptr;
     std::size_t maxTrials = 1;
     if (useSteadyState) {
         PopulationPlotExperiment<SteadyStateMOCMA> experiment;
-        experiment.run(mu, n, maxEvaluations, reference_ptr, individual, extra, instance, maxTrials);
+        experiment.run(seed, mu, n, maxEvaluations, reference_ptr, individual, extra, instance, maxTrials);
     } else {
         PopulationPlotExperiment<MOCMA> experiment;
-        experiment.run(mu, n, maxEvaluations, reference_ptr, individual, extra, instance, maxTrials);
+        experiment.run(seed, mu, n, maxEvaluations, reference_ptr, individual, extra, instance, maxTrials);
     }
 }
