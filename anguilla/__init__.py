fbf4f190a5559477bcf7612ea687a66d1f6f4cf4
Anguilla anguilla <pocs.anguilla@gmail.com>
2021-02-05 13:49:10 +0100

    Bump version: 0.0.11 -> 0.0.12

--- anguilla/__init__.py
+++ anguilla/__init__.py
@@ -7,4 +7,4 @@ References
 """
 
 __author__ = "Anguilla Development Team"
-__version__ = "0.0.11"
+__version__ = "0.0.12"
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from skbuild import setup
 
 def main():
     author = "Anguilla Development Team"
-    version = "0.0.11"
+    version = "0.0.12"
     with open("README.rst", encoding="utf-8") as f:
         long_description = f.read()
 
