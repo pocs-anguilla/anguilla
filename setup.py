8996bc5ff23abbd50cc93c03daf5e26fc32b3e4f
Anguilla anguilla <pocs.anguilla@gmail.com>
2021-02-18 17:34:55 +0100

    Bump version: 0.0.16 -> 0.0.17

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from skbuild import setup
 
 def main():
     author = "Anguilla Development Team"
-    version = "0.0.16"
+    version = "0.0.17"
     with open("README.rst", encoding="utf-8") as f:
         long_description = f.read()
 
