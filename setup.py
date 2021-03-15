0d40e5a84c426ea86dee372b8a985609fde3b154
Anguilla anguilla <pocs.anguilla@gmail.com>
2021-03-15 04:12:46 +0100

    Add work-around for BLAS dependency

--- setup.py
+++ setup.py
@@ -3,7 +3,19 @@
 # Adapted from the reference example for Pybind11 + Scikit-build:
 # https://github.com/pybind/scikit_build_example/blob/master/setup.py
 
+import os
 from skbuild import setup
+
+# Temporary work-around for building PyPI package
+# with the BLAS compatible library BLIS.
+# TODO: figure out if it is possible to use same BLAS as Numpy.
+def compute_install_requires():
+    deps = [
+        "numpy >= 1.20",
+    ]
+    if os.getenv("CONDA_BUILD", None) is None:
+        deps.append("blis >= 0.7.4")
+    return deps
 
 
 def main():
@@ -38,20 +50,30 @@ def main():
             "-DCMAKE_CXX_EXTENSIONS=OFF",
         ],
         python_requires=">3.7",
-        install_requires=[
-            "numpy >= 1.20",
-        ],
+        install_requires=compute_install_requires(),
         extras_require={
             ':python_version <= "3.7"': [
                 "typing_extensions",
             ],
         },
         classifiers=[
-            "Topic :: Scientific/Engineering",
-            "Programming Language :: Python :: 3 :: Only",
-            "Programming Language :: C++",
-            "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
             "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+            "Programming Language :: C++",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Scientific/Engineering",
+            "Typing :: Typed",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
         ],
     )
 
